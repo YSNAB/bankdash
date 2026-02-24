f7bae551c8bd9c86dc3a54c587c1d16ae4d46cd4
Yassine <yassabdell@hotmail.com>
2026-02-24 10:47:53 +0100

    removed dev in metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "DEV Phonebank Admin",
+  title: "Phonebank Admin",
   description: "Phonebank Admin Dashboard",
 };
 
